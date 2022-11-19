c93714cbed6ba9a94cb92e508e325807bcdac855
Jon Brule <brulejr@gmail.com>
2022-11-19 12:13:55 -0500

    Moves i18n translations to jsonserver db.json file for dev

--- src/i18n.js
+++ src/i18n.js
@@ -1,98 +1,15 @@
 import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
-const resources = {
-  en: {
-    translation: {
-      page: {
-        framework: {
-          title: 'Task Tracker',
-          buttons: {
-            add: 'Add',
-            close: 'Close'
-          },
-          links: {
-            about: 'About',
-            returnToHome: 'Go Back'
-          },
-          copyright: 'Copyright \u00A9 2022'
-        },
-        About: {
-          version: 'Version {{version}}'
-        },
-        AddTask: {
-          fields: {
-            task: {
-              label: 'Task',
-              placeholder: 'Add Task'
-            },
-            day: {
-              label: 'Day \u0026 Time',
-              placeholder: 'Add Day \u0026 Time'
-            },
-            reminder: {
-              label: 'Reminder'
-            }
-          },
-          buttons: {
-            saveTask: 'Save Task'
-          },
-          validation: {
-            taskRequired: 'Please add a task'
-          }
-        }
-      }
-    }
-  },
-  es: {
-    translation: {
-      page: {
-        framework: {
-          title: 'Rastreador de Tareas',
-          buttons: {
-            add: 'Agregar',
-            close: 'Cerrar'
-          },
-          links: {
-            about: 'Sobre',
-            returnToHome: 'Regresa'
-          },
-          copyright: 'Derechos de Autor \u00A9 2022'
-        },
-        About: {
-          version: 'Version {{version}}'
-        },
-        AddTask: {
-          fields: {
-            task: {
-              label: 'Tarea',
-              placeholder: 'Agregar Tarea'
-            },
-            day: {
-              label: 'Dia y Hora',
-              placeholder: 'Agregar Dia y Hora'
-            },
-            reminder: {
-              label: 'Recordatorio'
-            }
-          },
-          buttons: {
-            saveTask: 'Guardar Tarea'
-          },
-          validation: {
-            taskRequired: 'Por favor agregue una tarea'
-          }
-        }
-      }
-    }
-  }
-}
-
 i18n
+  .use(Backend)
   .use(initReactI18next)
   .init({
-    resources,
-    lng: 'en',
+    backend: {
+      loadPath: 'http://localhost:5000/locales/{{lng}}'
+    },
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false
     }
